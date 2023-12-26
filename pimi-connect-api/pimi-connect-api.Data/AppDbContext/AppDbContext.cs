568bd296e06db354b29686674ca2e00f74e2a78b
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2023-12-26 15:07:11 +0100

    Added DbContext and created first migrations

---
+++ pimi-connect-api/pimi-connect-api.Data/AppDbContext/AppDbContext.cs
@@ -0,0 +1,72 @@
+using Microsoft.EntityFrameworkCore;
+using pimi_connect_app.Data.Entities;
+
+namespace pimi_connect_app.Data.AppDbContext;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<AuthEntity> AuthEntities { get; set; }
+    public DbSet<UserChatEntity> UserChats { get; set; }
+    public DbSet<AttachmentEntity> Attachments { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        CreateUsers(modelBuilder);
+        CreateAuthEntities(modelBuilder);
+        CreateUserChats(modelBuilder);
+        CreateAttachments(modelBuilder);
+    }
+    
+    private static void CreateUsers(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserEntity>(ef =>
+        {
+            ef.Property(u => u.Id)
+                .ValueGeneratedOnAdd();
+        });
+
+        modelBuilder.Entity<UserEntity>()
+            .Property(u => u.UserName)
+            .IsRequired()
+            .HasMaxLength(50);
+        
+        modelBuilder.Entity<UserEntity>()
+            .Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        modelBuilder.Entity<UserEntity>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+    }
+    
+    private static void CreateAuthEntities(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserEntity>(ef =>
+        {
+            ef.Property(u => u.Id)
+                .ValueGeneratedOnAdd();
+        });
+    }
+    
+    private static void CreateUserChats(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserChatEntity>(ef =>
+        {
+            ef.Property(u => u.Id)
+                .ValueGeneratedOnAdd();
+        });
+    }
+    
+    private static void CreateAttachments(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AttachmentEntity>(ef =>
+        {
+            ef.Property(u => u.Id)
+                .ValueGeneratedOnAdd();
+        });
+    }
+}
