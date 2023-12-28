a1f4bea726ad5ec5a6deb87d6068583e2dd882b4
Daniel Śliwka <danielsliwkado@gmail.com>
2023-12-28 16:18:03 +0100

    Added enum, entities and models for Chat, Message, UserChat

--- pimi-connect-api/pimi-connect-api.Data/Entities/UserChatEntity.cs
+++ pimi-connect-api/pimi-connect-api.Data/Entities/UserChatEntity.cs
@@ -1,7 +1,18 @@
-﻿namespace pimi_connect_app.Data.Entities
+﻿using pimi_connect_app.Data.Enums;
+
+namespace pimi_connect_app.Data.Entities
 {
     public class UserChatEntity
     {
-        public Guid Id { get; set; }
+        public string NickName { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ChatId { get; set; }
+        public Guid LastReadMessageId { get; set; }
+        public Guid UserKeyId { get; set; }
+        public ChatRole Role { get; set; }
+        public MessageEntity? LastReadMessage { get; set; }
+        public UserEntity? User { get; set; }
+        public ChatEntity? Chat { get; set; }
+
     }
 }
---
+++ pimi-connect-api/pimi-connect-api.Data/Models/MessageDto.cs
@@ -0,0 +1,20 @@
+﻿using pimi_connect_app.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pimi_connect_app.Data.Models
+{
+    public class MessageDto
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Guid ChatId { get; set; }
+        public Guid UserCreatedId { get; set; }
+        public Guid AttachmentId { get; set; }
+        public Guid IdPasswordContainer { get; set; }
+    }
+}
