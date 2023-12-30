1d15525fda6cfb4a2ffe5776330d3079ccf15043
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2023-12-30 18:26:06 +0100

    Created GlobalUsings.cs

---
+++ pimi-connect-api/pimi-connect-api.UnitTests/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using Xunit;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
+
+global using pimi_connect_api.Controllers;
+global using pimi_connect_api.Services;
+global using static pimi_connect_api.UnitTests.Shared.DataGenerator;
