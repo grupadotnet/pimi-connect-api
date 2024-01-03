360cf4f9f2e6ddce942748d2a3b8a1a88044f167
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-01-03 19:36:14 +0100

    Finished writing tests for UserController

--- pimi-connect-api/pimi-connect-api.UnitTests/ControllersTests/UserControllerTests.cs
+++ pimi-connect-api/pimi-connect-api.UnitTests/ControllersTests/UserControllerTests.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using pimi_connect_api.Exceptions;
 using pimi_connect_api.UnitTests.Base;
-using pimi_connect_app.Data.Enums;
-using pimi_connect_app.Data.Models;
 
 namespace pimi_connect_api.UnitTests.ControllersTests;
 
@@ -16,7 +14,7 @@ public class UserControllerTests : ControllerUnitTestsBase<UserDto>
 
         if (fillDb)
         {
-            await Helper.FillUsersTable(EntitiesCount);
+            await Helper.FillUsersTable(ExistingIds);
         }
         
         // initialize Controller
@@ -24,38 +22,42 @@ public class UserControllerTests : ControllerUnitTestsBase<UserDto>
         _userController = new UserController(userService);
     }
     
-    protected override UserDto CreateDto(int i)
+    [Theory]
+    [InlineData(true)] // should successfully return user with the given id
+    [InlineData(false)] // should return status 404
+    public async void GetAsyncTest(bool idExists)
     {
-        return new UserDto()
+        // Arrange
+        await Arrange();
+    
+        // Act && Assert
+        if (idExists)
         {
-            UserName = GenerateUserName(i),
-            Email = GenerateEmail(i),
-            Status = UserStatus.Accessible,
-            ProfilePictureKey = GenerateGuid()
-        };
+            var result = await _userController.GetAsync(ExistingId);
+            var resultTuple = GetStatusAndContentFromResult(result);
+            
+            Assert.NotNull(resultTuple.Content);
+            Assert.Equal(ExistingId, resultTuple.Content.Id);
+            Assert.Equal(StatusCodes.Status200OK, resultTuple.Status);
+        }
+        else
+        {
+            await Assert.ThrowsAsync<NotFound404Exception>(() => 
+                _userController.GetAsync(NotExistingId));
+        }
     }
     
-    // [Theory]
-    // [InlineData(false, false)] 
-    // public async void GetAsyncTest()
-    // {
-    //     // Arrange
-    //     await Arrange();
-    //
-    //     // Act && Assert
-    //     
-    //     
-    // }
-    
     [Theory]
-    [InlineData(true)] 
-    [InlineData(false)] 
+    [InlineData(true)] // should successfully return the user with given email
+    [InlineData(false)] // should return status 404
     public async void GetByEmailAsyncTest(bool emailExists)
     {
         // Arrange
         await Arrange();
 
-        var email = emailExists ? GenerateEmail(ExistingIndex) : GenerateEmail(NotExistingIndex);
+        var email = emailExists ? 
+            GenerateEmail(ExistingId) : 
+            GenerateEmail(NotExistingId);
         
         // Act && Assert
         if (emailExists)
@@ -64,71 +66,154 @@ public class UserControllerTests : ControllerUnitTestsBase<UserDto>
             var resultTuple = GetStatusAndContentFromResult(result);
             
             Assert.NotNull(resultTuple.Content);
+            Assert.Equal(email, resultTuple.Content.Email);
             Assert.Equal(StatusCodes.Status200OK, resultTuple.Status);
         }
         else
         {
-            await Assert.ThrowsAsync<NotFound404Exception>(() => _userController.GetByEmailAsync(email));
+            await Assert.ThrowsAsync<NotFound404Exception>(() => 
+                _userController.GetByEmailAsync(email));
         }
     }
     
     [Theory]
-    [InlineData(false, false)] 
-    public async void GetAllAsyncTest()
+    [InlineData(true)] // should return all dtos from db
+    [InlineData(false)] // should return an empty list
+    public async void GetAllAsyncTest(bool hasUsersInDb)
     {
         // Arrange
-        
+        await Arrange(hasUsersInDb);
+
         // Act && Assert
+        var result = await _userController.GetAllAsync();
+        var resultTuple = GetStatusAndListFromResult(result);
+        
+        Assert.NotNull(resultTuple.List);
+        
+        if (hasUsersInDb)
+        {
+            Assert.Equal(EntitiesCount, resultTuple.List.Count);
+        }
+        else
+        {
+            Assert.Equal(new List<UserDto>(), resultTuple.List);
+        }
     }
     
-    // [Theory]
-    // [InlineData(false, false)] 
-    // public async void UpdateAsyncTest()
-    // {
-    //     // Arrange
-    //     
-    //     // Act && Assert
-    // }
-    
     [Theory]
-    [InlineData(false)]
-    [InlineData(true)]
-    public async void AddAsyncTest(bool emailAlreadyExists)
+    [InlineData(true)] // should return successfully updated UserDto
+    [InlineData(false)] // should return status 404
+    public async void UpdateAsyncTest(bool userExists)
     {
         // Arrange
         await Arrange();
 
-        var userToAdd = emailAlreadyExists ? CreateDto(ExistingIndex) : CreateDto(NotExistingIndex);
-        
+        var newUserName = "updated";
+
+        var userToUpdate = GenerateUserDto(Mapper, userExists ? ExistingId : NotExistingId);
+        userToUpdate.UserName = newUserName;
+
         // Act && Assert
-        if (!emailAlreadyExists)
+        if (userExists)
         {
-            var result = await _userController.AddAsync(userToAdd);
+            var result = await _userController.UpdateAsync(userToUpdate);
             var resultTuple = GetStatusAndContentFromResult(result);
-
-            Assert.NotNull(resultTuple.Content);
-
-            var addedUser = await TestDbContext
+            
+            var userInDb = await TestDbContext
                 .Users
                 .AsNoTracking()
-                .FirstOrDefaultAsync(u => u.Id == resultTuple.Content.Id);
-
-            Assert.NotNull(addedUser);
-            Assert.Equal(userToAdd.Email, addedUser.Email);
+                .FirstOrDefaultAsync(u => u.Id == userToUpdate.Id);
+            
+            Assert.NotNull(resultTuple.Content);
+            Assert.NotNull(userInDb);
+            
+            Assert.Equal(userToUpdate.UserName, resultTuple.Content.UserName);
+            Assert.Equal(userToUpdate.UserName, userInDb.UserName);
+            
             Assert.Equal(StatusCodes.Status200OK, resultTuple.Status);
         }
         else
         {
-            await Assert.ThrowsAsync<BadRequest400Exception>(() => _userController.AddAsync(userToAdd));
+            await Assert.ThrowsAsync<NotFound404Exception>(() => 
+                _userController.UpdateAsync(userToUpdate));
+        }
+        
+    }
+    
+    [Theory]
+    [InlineData(false, false)] // should successfully add user into the db
+    [InlineData(true, false)] // should return status 400
+    [InlineData(false , true)] // should return status 400
+    [InlineData(true , true)] // should return status 400
+    public async void AddAsyncTest(bool idAlreadyExists, bool emailAlreadyExists)
+    {
+        // Arrange
+        await Arrange();
+
+        var userToAdd = new UserDto()
+        {
+            Id = idAlreadyExists ? ExistingId : NotExistingId,
+            UserName = GenerateUserName(),
+            Email = GenerateEmail(emailAlreadyExists ? ExistingId : NotExistingId),
+            Status = UserStatus.Accessible,
+            ProfilePictureKey = Guid.NewGuid()
+        };
+        
+        // Act && Assert
+        if (!idAlreadyExists && !emailAlreadyExists)
+        {
+            var result = await _userController.AddAsync(userToAdd);
+            var resultTuple = GetStatusAndContentFromResult(result);
+
+            var userInDb = await TestDbContext
+                .Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Id == userToAdd.Id);
+            
+            Assert.NotNull(resultTuple.Content);
+            Assert.NotNull(userInDb);
+            
+            Assert.Equal(userToAdd.UserName, resultTuple.Content.UserName);
+            Assert.Equal(userToAdd.UserName, userInDb.UserName);
+            
+            Assert.Equal(userToAdd.Email, resultTuple.Content.Email);
+            Assert.Equal(userToAdd.Email, userInDb.Email);
+            
+            Assert.Equal(StatusCodes.Status200OK, resultTuple.Status);
+        }
+        else
+        {
+            await Assert.ThrowsAsync<BadRequest400Exception>(() => 
+                _userController.AddAsync(userToAdd));
         }
     }
     
-    // [Theory]
-    // [InlineData(false, false)] 
-    // public async void DeleteAsyncTest()
-    // {
-    //     // Arrange
-    //     
-    //     // Act && Assert
-    // }
+    [Theory]
+    [InlineData(true)] // should successfully delete user from the db and return status 204
+    [InlineData(false)] // should return status 404
+    public async void DeleteAsyncTest(bool userExists)
+    {
+        // Arrange
+        await Arrange();
+
+        // Act && Assert
+        if (userExists)
+        {
+            var result = await _userController.DeleteAsync(ExistingId);
+            var resultTuple = GetStatusAndContentFromResult(result);
+
+            var userInDb = await TestDbContext
+                .Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Id == ExistingId);
+            
+            Assert.Equal(null, userInDb);
+            Assert.Equal(StatusCodes.Status204NoContent, resultTuple.Status);
+        }
+        else
+        {
+            await Assert.ThrowsAsync<NotFound404Exception>(() => 
+                _userController.DeleteAsync(NotExistingId));
+        }
+    }
 }
