78331df2e52b0d0035a134ae5a362807b324cc4a
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-01-03 19:33:12 +0100

    Changed DataGenerator to use Guid instead of int id

--- pimi-connect-api/pimi-connect-api.UnitTests/Shared/DataGenerator.cs
+++ pimi-connect-api/pimi-connect-api.UnitTests/Shared/DataGenerator.cs
@@ -1,20 +1,41 @@
+using AutoMapper;
+
 namespace pimi_connect_api.UnitTests.Shared;
 
 public static class DataGenerator
 {
-    public static Guid GenerateGuid()
-    {
-        return new Guid();
-    }
+    #region Generate User related data
 
-    public static string GenerateUserName(int i = -1)
+    public static string GenerateUserName(Guid id = new ())
     {
-        return $"user{i}";
+        return $"user{id}";
     }
     
-    public static string GenerateEmail(int i = -1)
+    public static string GenerateEmail(Guid id = new ())
     {
-        return $"user{i}@domain.com";
+        return $"user{id}@domain.com";
     }
+    
+    public static UserEntity GenerateUserEntity(Guid id = new ())
+    {
+        return new UserEntity()
+        {
+            Id = id,
+            UserName = GenerateUserName(id),
+            Email = GenerateEmail(id),
+            Status = UserStatus.Accessible,
+            ProfilePictureKey = Guid.NewGuid()
+        };
+    }
+    
+    public static UserDto GenerateUserDto(IMapper mapper, Guid id = new ())
+    {
+        var userEntity = GenerateUserEntity(id);
+
+        return mapper.Map<UserDto>(userEntity);
+    }
+
+    #endregion
+    
     
 }
