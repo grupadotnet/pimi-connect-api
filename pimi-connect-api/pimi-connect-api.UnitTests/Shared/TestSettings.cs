895ab6289d8d729c277295b16182ef187a655c68
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-01-04 09:46:38 +0100

    Changed Tests configuration to use new appsettings.Tests.json

--- pimi-connect-api/pimi-connect-api.UnitTests/Shared/TestSettings.cs
+++ pimi-connect-api/pimi-connect-api.UnitTests/Shared/TestSettings.cs
@@ -2,7 +2,7 @@ namespace pimi_connect_api.UnitTests.Shared;
 
 public class TestSettings
 {
-    protected const string ConfigurationFileName = "appsettings.json";
+    protected const string ConfigurationFileName = "appsettings.Tests.json";
     protected const string ConnectionStringName = "pimi-connect-postgresql-test";
     protected const string MigrationsAssemblyName = "pimi-connect-api.API";
     protected const int EntitiesCount = 6;
