bbd9e148f0ffb425d5c9fdb284c4846b8cbdac57
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-01-13 12:00:48 +0100

    Created React App for development of signalR

---
+++ pimi-connect-api/pimi-connect-api.API/Hubs/ChatHub.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using pimi_connect_app.Data.AppDbContext;
+using pimi_connect_app.Data.Models;
+using pimi_connect_app.Data.Models.ConnectionModels;
+
+namespace pimi_connect_api.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public static int TotalUsers { get; set; } = 0;
+        
+        private readonly AppDbContext _dbContext;
+        
+        public ChatHub(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        // public override Task OnConnectedAsync()
+        // {
+        //     TotalUsers++;
+        //     Clients.All.SendAsync("showMessageUserConnected", TotalUsers).GetAwaiter().GetResult();
+        //     return base.OnConnectedAsync();
+        // }
+        //
+        // public override Task OnDisconnectedAsync(Exception? exception)
+        // {
+        //     TotalUsers--;
+        //     Clients.All.SendAsync("showMessageUserDisconnected", TotalUsers).GetAwaiter().GetResult();
+        //     return base.OnDisconnectedAsync(exception);
+        // }
+
+        public async Task JoinChat(UserConnection conn)
+        {
+            TotalUsers++;
+            await Clients.All
+                .SendAsync("ReceiveMessage", "admin", 
+                    $"{conn.UserName} has joined!");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection conn)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
+            
+            await Clients.Group(conn.ChatRoom)
+                .SendAsync("ReceiveMessage", "admin", 
+                    $"{conn.UserName} has joined {conn.ChatRoom}");
+        }
+
+        // public async Task SendMessage(string msg)
+        // {
+        //     await Clients.Groups(U)
+        // }
+    }
+}
