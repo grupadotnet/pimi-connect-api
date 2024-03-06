8e3a492cf529d29254304eb85bf1a37e6d0458e2
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-03-06 22:33:44 +0100

    Created UserConnectionDto for connecting with react app client

--- pimi-connect-api/pimi-connect-api.Data/Models/UserConnectionDto.cs
+++ pimi-connect-api/pimi-connect-api.Data/Models/UserConnectionDto.cs
@@ -2,6 +2,6 @@ namespace pimi_connect_app.Data.Models.ConnectionModels;
 
 public class UserConnectionDto
 {
-    public string UserName { get; set; }
-    public string ChatRoom { get; set; }
+    public string Username { get; set; }
+    public string Chatroom { get; set; }
 }
