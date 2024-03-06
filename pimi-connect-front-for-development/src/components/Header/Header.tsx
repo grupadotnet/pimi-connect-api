9f3e1e77845864a23072bc7e1808226aff20bba2
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-03-06 22:28:06 +0100

    Fixed header bug

--- pimi-connect-front-for-development/src/components/Header/Header.tsx
+++ pimi-connect-front-for-development/src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import {Col, Row} from "react-bootstrap";
 export function Header() {
     return (
         <>
-            <Row class="px-5 my-5">
+            <Row className="px-5 pt-5">
                 <Col sm="12">
                     <h1 className="font-weight-light">Test frontend for SignalR</h1>
                 </Col>
