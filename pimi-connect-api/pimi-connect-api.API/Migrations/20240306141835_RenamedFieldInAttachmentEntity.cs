49d48b8ee280f670afde316b5ebd5c2eb6ee5ede
ThePanToster <alcrafter@o2.pl>
2024-03-06 15:24:15 +0100

    Add migration

---
+++ pimi-connect-api/pimi-connect-api.API/Migrations/20240306141835_RenamedFieldInAttachmentEntity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pimi_connect_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedFieldInAttachmentEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "publicName",
+                table: "Attachments",
+                newName: "PublicName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublicName",
+                table: "Attachments",
+                newName: "publicName");
+        }
+    }
+}
